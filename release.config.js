5551ec20ec9b85560dbf7bc7ce560bf0c9ab25fc
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-28 10:43:20 +0200

    ci(release): switch to Maven Central Portal
     Conflicts:
    	gradle/libs.versions.toml

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 var publishCmd = `
-./gradlew uploadAll releaseStagingRepositoryOnMavenCentral || exit 1
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
 ./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
 `
